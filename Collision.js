a27be8dbc0767eb8ef39fe7e4650861c004db5c3
Dominic Orchard <dom.orchard@gmail.com>
2021-03-18 14:03:19 +0000

    formatting

--- Collision.js
+++ Collision.js
@@ -7,7 +7,7 @@ var numCircles = 10;
 var rad = 40; // radius
 var maxSpeed = 4;
 
-var frameDelay = 1000/30;
+var frameDelay = 1000 / 30;
 var unpaused = true;
 
 // Control flags
@@ -30,166 +30,168 @@ var runs = 0;
 var totalTime = 0;
 
 function setup() {
-    // Set up event handlers
-    document.body.addEventListener("keypress", pause);
+	// Set up event handlers
+	document.body.addEventListener("keypress", pause);
 
-    // Set up the form
-    document.getElementById("circles").value = numCircles;
-    document.getElementById("radius").value = rad;
-    document.getElementById("maxObjs").value = maxObjectsInQuadrant;
-    setupMain();
+	// Set up the form
+	document.getElementById("circles").value = numCircles;
+	document.getElementById("radius").value = rad;
+	document.getElementById("maxObjs").value = maxObjectsInQuadrant;
+	setupMain();
 
-    // Trigger the main loop
-    setTimeout(render, frameDelay);
+	// Trigger the main loop
+	setTimeout(render, frameDelay);
 }
 
 function setupMain() {
-    // Wipe the circle state
-    circles = [];
+	// Wipe the circle state
+	circles = [];
 
-    // Set up the canvas
-    var canvas = document.getElementById("canvas");
-    canvas.setAttributeNS(null, 'height', height);
-    canvas.setAttributeNS(null, 'width', width);
-    for (var i = 0; i < numCircles; i++) {
-	// Creat the circle
-	let circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	let x = Math.round(Math.random()*width);
-	let y = Math.round(Math.random()*height);
-	circ.setAttributeNS(null, 'cx', x);
-	circ.setAttributeNS(null, 'cy', y);
-	circ.setAttributeNS(null, 'r', rad);
-	circ.setAttributeNS(null, 'style', "fill:" + normalColor);
-	canvas.appendChild(circ);
+	// Set up the canvas
+	var canvas = document.getElementById("canvas");
+	canvas.setAttributeNS(null, 'height', height);
+	canvas.setAttributeNS(null, 'width', width);
+	for (var i = 0; i < numCircles; i++) {
+		// Creat the circle
+		let circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		let x = Math.round(Math.random() * width);
+		let y = Math.round(Math.random() * height);
+		circ.setAttributeNS(null, 'cx', x);
+		circ.setAttributeNS(null, 'cy', y);
+		circ.setAttributeNS(null, 'r', rad);
+		circ.setAttributeNS(null, 'style', "fill:" + normalColor);
+		canvas.appendChild(circ);
 
-	// Create a label but these may get hidden
-	let txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	txt.setAttributeNS(null, 'x', -100);
-	txt.setAttributeNS(null, 'y', -100);
-	txt.innerHTML = i;
-	canvas.appendChild(txt);
+		// Create a label but these may get hidden
+		let txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		txt.setAttributeNS(null, 'x', -100);
+		txt.setAttributeNS(null, 'y', -100);
+		txt.innerHTML = i;
+		canvas.appendChild(txt);
 
-	// Randomise the velocity
-	circles.push({circ: circ, txt: txt,
-		      xv: Math.round(Math.random()*maxSpeed),
-		      yv: Math.round(Math.random()*maxSpeed)});
-    }
+		// Randomise the velocity
+		circles.push({
+			circ: circ, txt: txt,
+			xv: Math.round(Math.random() * maxSpeed),
+			yv: Math.round(Math.random() * maxSpeed)
+		});
+	}
 }
 
 // Called when the 'Restart' button is hit
 function updateCircles() {
-    totalTime = 0;
-    runs = 0;
-    rad = parseInt(document.getElementById("radius").value);
-    numCircles = parseInt(document.getElementById("circles").value);
-    maxObjectsInQuadrant = parseInt(document.getElementById("maxObjs").value);
-    methodNew = document.getElementById("panel").elements["method"].value;
+	totalTime = 0;
+	runs = 0;
+	rad = parseInt(document.getElementById("radius").value);
+	numCircles = parseInt(document.getElementById("circles").value);
+	maxObjectsInQuadrant = parseInt(document.getElementById("maxObjs").value);
+	methodNew = document.getElementById("panel").elements["method"].value;
 
-    // Remove all the old circles
-    var canvas = document.getElementById("canvas");
-    while (canvas.children.length > 0) {
-	canvas.removeChild(canvas.firstElementChild);
-    }
-    // Reboot
-    setupMain();
+	// Remove all the old circles
+	var canvas = document.getElementById("canvas");
+	while (canvas.children.length > 0) {
+		canvas.removeChild(canvas.firstElementChild);
+	}
+	// Reboot
+	setupMain();
 
 }
 
 // When the check boxes are edited, do the these updates
 function updateControls() {
-    labels = document.getElementById('labels').checked;
-    quadTree = document.getElementById('quad').checked;
+	labels = document.getElementById('labels').checked;
+	quadTree = document.getElementById('quad').checked;
 
-    if (!labels) {
-	// Move all the labels away
-	for (var i = 0; i < circles.length; i++) {
-	    circles[i].txt.x.baseVal[0].value = -100
+	if (!labels) {
+		// Move all the labels away
+		for (var i = 0; i < circles.length; i++) {
+			circles[i].txt.x.baseVal[0].value = -100
+		}
 	}
-    }
 }
 
 // Pause handler
 function pause(event) {
-    if (event.key == "p") {
-	unpaused = !unpaused;
-    }
-    if (!unpaused) {
-	document.getElementById("status").innerHTML =
-	    "<span style='color:red'>Paused</span><br />"
-	  + document.getElementById("status").innerHTML;
-    }
+	if (event.key == "p") {
+		unpaused = !unpaused;
+	}
+	if (!unpaused) {
+		document.getElementById("status").innerHTML =
+			"<span style='color:red'>Paused</span><br />"
+			+ document.getElementById("status").innerHTML;
+	}
 }
 
-function render(){
-    var start = new Date().getTime();
-    var time = 0;
+function render() {
+	var start = new Date().getTime();
+	var time = 0;
 
-    if (unpaused) {
-	root = newQuadTreeRoot();
+	if (unpaused) {
+		root = newQuadTreeRoot();
 
-	for (var i = 0; i < circles.length; i++) {
+		for (var i = 0; i < circles.length; i++) {
 
-	    let xv = circles[i].xv;
-	    let yv = circles[i].yv;
+			let xv = circles[i].xv;
+			let yv = circles[i].yv;
 
-	    // Move the circles
-	    circles[i].circ.cx.baseVal.value =
-		(circles[i].circ.cx.baseVal.value + xv) % width;
+			// Move the circles
+			circles[i].circ.cx.baseVal.value =
+				(circles[i].circ.cx.baseVal.value + xv) % width;
 
-	    circles[i].circ.cy.baseVal.value =
-  		(circles[i].circ.cy.baseVal.value + yv) % height;
+			circles[i].circ.cy.baseVal.value =
+				(circles[i].circ.cy.baseVal.value + yv) % height;
 
-	    // Move the labels if we are viewing labels
-	    if (labels) {
-				circles[i].txt.x.baseVal[0].value = circles[i].circ.cx.baseVal.value - (rad/2);
-	        circles[i].txt.y.baseVal[0].value = circles[i].circ.cy.baseVal.value + (rad/2);
-	    }
+			// Move the labels if we are viewing labels
+			if (labels) {
+				circles[i].txt.x.baseVal[0].value = circles[i].circ.cx.baseVal.value - (rad / 2);
+				circles[i].txt.y.baseVal[0].value = circles[i].circ.cy.baseVal.value + (rad / 2);
+			}
 
-	    // Add into quad tree if we are using this method
-	    if (method == "fast") {
+			// Add into quad tree if we are using this method
+			if (method == "fast") {
 				insert(circles[i], root);
-	    }
+			}
+		}
+
+		clearState();
+
+		// Draw quad tree if requested
+		if (method == "fast" && quadTree) {
+			drawQuadTree(canvas, root, 0);
+		}
+
+		checkCollisions(circles);
+
+		// Show the time taken
+		var end = new Date().getTime();
+		time = end - start;
+		totalTime += time;
+		runs++;
+		document.getElementById("speed").innerHTML = (totalTime / runs).toFixed(1) + "ms";
+		method = methodNew;
 	}
 
-	clearState();
-
-	// Draw quad tree if requested
-	if (method == "fast" && quadTree) {
-	    drawQuadTree(canvas, root, 0);
+	var delay = frameDelay - time;
+	if (delay < 0) {
+		delay = 10;
 	}
-
-	checkCollisions(circles);
-
-	// Show the time taken
-	var end = new Date().getTime();
-	time = end - start;
-	totalTime += time;
-	runs++;
-	document.getElementById("speed").innerHTML = (totalTime / runs).toFixed(1) + "ms";
-	method = methodNew;
-    }
-
-    var delay = frameDelay - time;
-    if (delay < 0) {
-	delay = 10;
-    }
-    setTimeout(render, delay);
+	setTimeout(render, delay);
 }
 
 // Reset various things that need resetting between frames,
 // including collision markers
 function clearState() {
-    document.getElementById("status").innerHTML = "";
-    var canvas = document.getElementById("canvas");
-    var oldRects = document.getElementsByTagName("rect");
-    for (var i = 0; i < oldRects.length; i++) {
-	canvas.removeChild(oldRects[i]);
-    }
-    for (var i = 0; i < circles.length; i++) {
-	circles[i].circ.setAttributeNS(null, 'style', "fill:" + normalColor);
-    }
+	document.getElementById("status").innerHTML = "";
+	var canvas = document.getElementById("canvas");
+	var oldRects = document.getElementsByTagName("rect");
+	for (var i = 0; i < oldRects.length; i++) {
+		canvas.removeChild(oldRects[i]);
+	}
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].circ.setAttributeNS(null, 'style', "fill:" + normalColor);
+	}
 }
 
 function makeCircleRed(circle) {
-    circle.circ.setAttributeNS(null, 'style', "fill:rgb(250,110,110,0.5)");
+	circle.circ.setAttributeNS(null, 'style', "fill:rgb(250,110,110,0.5)");
 }
